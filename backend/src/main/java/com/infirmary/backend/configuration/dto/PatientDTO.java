930d06421033182d72abd0e56d7bc509ade8186b
vibzz2003 <500093004@stu.upes.ac.in>
2024-08-30 14:30:04 +0530

    CD-001: feat(patient): * Patient DTO, model class created

---
+++ backend/src/main/java/com/infirmary/backend/configuration/dto/PatientDTO.java
@@ -0,0 +1,33 @@
+package com.infirmary.backend.configuration.dto;
+
+import com.infirmary.backend.configuration.model.Patient;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.nio.file.SecureDirectoryStream;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PatientDTO {
+    private Long sapId;
+    private String name;
+    private String school;
+    private String dateOfBirth;
+    private String program;
+    private String phoneNumber;
+    private String address;
+    private String emergencyContact;
+
+    public PatientDTO(Patient patient){
+        this.sapId = patient.getSapId();
+        this.name = patient.getName();
+        this.school = patient.getSchool();
+        this.dateOfBirth = patient.getDateOfBirth();
+        this.program = patient.getProgram();
+        this.phoneNumber = patient.getPhoneNumber();
+        this.address = patient.getAddress();
+        this.emergencyContact = patient.getEmergencyContact();
+    }
+}
