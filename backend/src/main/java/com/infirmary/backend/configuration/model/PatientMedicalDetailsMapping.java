7057c33db6971ce2dc1efe6958af328bf94055c0
vibzz2003 <500093004@stu.upes.ac.in>
2024-08-30 14:30:51 +0530

    CD-001: feat(patient): * MedicalDetails and patient mapping done

---
+++ backend/src/main/java/com/infirmary/backend/configuration/model/PatientMedicalDetailsMapping.java
@@ -0,0 +1,32 @@
+package com.infirmary.backend.configuration.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "patient_medical_details_mapping")
+@NoArgsConstructor
+public class PatientMedicalDetailsMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "patient_sap_id", referencedColumnName = "sap_id", nullable = false)
+    private Patient patient;
+
+    @OneToOne
+    @JoinColumn(name = "medical_details_id", referencedColumnName = "id", nullable = false)
+    private MedicalDetails medicalDetails;
+
+    public PatientMedicalDetailsMapping(Patient patient, MedicalDetails medicalDetails){
+        this.patient = patient;
+        this.medicalDetails = medicalDetails;
+    }
+}
