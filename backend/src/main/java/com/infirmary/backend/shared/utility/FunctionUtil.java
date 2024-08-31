a989f9bec57a4d91d2ec90931031431285c047bd
vibzz2003 <500093004@stu.upes.ac.in>
2024-09-01 00:05:39 +0530

    CD-001: feat(patient): * Added FunctionUtil for validate functions

---
+++ backend/src/main/java/com/infirmary/backend/shared/utility/FunctionUtil.java
@@ -0,0 +1,30 @@
+package com.infirmary.backend.shared.utility;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.apache.commons.lang3.StringUtils;
+import java.util.Objects;
+
+@Component
+public class FunctionUtil {
+    private static final Long MIN_NAME_LENGTH = 2L;
+
+    public static <T> ResponseEntity<?> createSuccessResponse(T data, HttpHeaders... header) {
+        if (header.length > 0) {
+            return new ResponseEntity<>(data, header[0], HttpStatus.OK);
+        }
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    public static boolean isNameInvalid(String name) {
+        return Objects.isNull(name)
+                || name.length() <= MIN_NAME_LENGTH
+                || StringUtils.containsAny(name,"[!@#$%&*()+=|<>?{}\\[\\]~]");
+    }
+
+    public static boolean isValidId(Long Id){
+        return Id.toString().matches("^5\\d{8}$");
+    }
+}
