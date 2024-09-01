3c0c36faf9a27abc158549ffa8f36608676271bf
vibzz2003 <500093004@stu.upes.ac.in>
2024-09-02 01:11:29 +0530

    CD-001: feat(patient): * PatientDetailsResponseDTO updated

--- backend/src/main/java/com/infirmary/backend/configuration/dto/PatientDetailsResponseDTO.java
+++ backend/src/main/java/com/infirmary/backend/configuration/dto/PatientDetailsResponseDTO.java
@@ -10,8 +10,11 @@ public class PatientDetailsResponseDTO {
     private PatientDTO patient;
 
     private MedicalDetailsDTO medicalDetails;
+
     public PatientDetailsResponseDTO(PatientDTO patient, MedicalDetailsDTO medicalDetails) {
+
         this.patient = patient;
+
         this.medicalDetails = medicalDetails;
     }
 }
