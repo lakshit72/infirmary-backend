a54d89ebd42fcccfda592efa9533ec097d9fc740
KartikeyaBishnoi <kartikeyabishnoi@gmail.com>
2024-09-10 15:06:29 +0530

    added @Configuration in AppointmentQueueManager.java

--- backend/src/main/java/com/infirmary/backend/shared/utility/AppointmentQueueManager.java
+++ backend/src/main/java/com/infirmary/backend/shared/utility/AppointmentQueueManager.java
@@ -1,10 +1,11 @@
 package com.infirmary.backend.shared.utility;
 
 import com.infirmary.backend.configuration.dto.AppointmentResDTO;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+@Configuration
 public class AppointmentQueueManager {
     private final Queue<Long> appointmentQueue;
 
